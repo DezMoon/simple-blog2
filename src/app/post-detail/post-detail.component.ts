9e17856a11fe8114b130902b1100f8aff1473f52
Desmond Moonga <moongachiku@gmail.com>
2024-01-30 16:39:11 +0200

    Fixed bug with unkown import

--- src/app/post-detail/post-detail.component.ts
+++ src/app/post-detail/post-detail.component.ts
@@ -3,6 +3,7 @@ import { Post } from '../models/post.model';
 
 @Component({
   selector: 'app-post-detail',
+
   templateUrl: './post-detail.component.html',
   styleUrls: ['./post-detail.component.css'],
 })
--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,35 +1,18 @@
-import { Component, Input } from '@angular/core';
+// post-list.component.ts
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Post } from '../models/post.model';
-import { Apollo } from 'apollo-angular';
-import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-post-list',
+
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css'],
 })
 export class PostListComponent {
-  @Input() posts!: Post[];
+  @Input() post?: Post[];
+  @Output() postSelected: EventEmitter<Post> = new EventEmitter<Post>();
 
-  constructor(private apollo: Apollo) {}
-
-  // Example: You can add more Apollo logic for fetching posts
-  fetchPosts() {
-    this.apollo
-      .watchQuery<any>({
-        query: gql`
-          query {
-            posts {
-              id
-              title
-              content
-              author
-            }
-          }
-        `,
-      })
-      .valueChanges.subscribe(({ data }) => {
-        this.posts = data.posts;
-      });
+  selectPost(post: Post) {
+    this.postSelected.emit(post);
   }
 }
